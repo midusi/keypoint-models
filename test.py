803902084248971511a3684ab01862b7560d8eba
pedroodb <dalbianco.pedro@gmail.com>
2022-05-11 11:25:17 -0300

    Beautify code

--- test.py
+++ test.py
@@ -35,21 +35,7 @@ dataset = LSA_Dataset(
     )
 dataset.label_transform = get_text_to_tensor_transform(dataset.vocab.__getitem__("<bos>"), dataset.vocab.__getitem__("<eos>"))
 
-print("Loading test dataset")
-dataset_test = LSA_Dataset(
-    root,
-    mode="test",
-    load_videos = load_videos,
-    load_keypoints = load_keypoints,
-    keypoints_transform = Compose([
-        keypoint_norm_to_center_transform,
-        get_frames_reduction_transform(max_frames)
-    ]),
-    keypoints_transform_each = get_keypoint_format_transform(keypoints_to_use)
-    )
-dataset_test.label_transform = get_text_to_tensor_transform(dataset_test.vocab.__getitem__("<bos>"), dataset_test.vocab.__getitem__("<eos>"))
-
-model = KeypointModel(max_frames, max(dataset.max_tgt_len, dataset_test.max_tgt_len) + 2, len(keypoints_to_use), len(dataset.vocab)).to(DEVICE)
+model = KeypointModel(max_frames, dataset.max_tgt_len + 2, len(keypoints_to_use), len(dataset.vocab)).to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
 
 #checkpoint = torch.load(CHECKPOINT_PATH / "checkpoint.tar")
